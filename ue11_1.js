f6cc9f2b8983b164dd87e416ad08f67ed937a350
Özlem Karahan <oezlem461@gmail.com>
2024-01-12 13:25:57 +0100

    uebung11

---
+++ ue11_1.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((anfrage, antwort) => {
+    const abfrageObjekt = url.parse(anfrage.url, true).query;
+
+    if (anfrage.method === 'GET' && abfrageObjekt.name) {
+        antwort.writeHead(200, {'Content-Type': 'text/plain'});
+        antwort.end(`Hallo ${abfrageObjekt.name}!`);
+    } else {
+        antwort.writeHead(404, {'Content-Type': 'text/plain'});
+        antwort.end('Keiner zum grueßen da');
+    }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Der Server läuft auf http://localhost:${PORT}`);
+});
